7663b19eb4c7aa1bb7dd4cd34ec551f6651db399
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-05-08 21:07:16 +0000

    Deploy annargrs/blog to github.com/annargrs/blog.git:gh-pages

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,55 @@
+var store = [{
+        "title": "Why blog about NLP in 2019?",
+        "excerpt":"When I said I wanted to start a research blog, I got asked why - wasn’t it something from the nineties that nobody did anymore? That’s actually not an unreasonable question: nobody is even able to keep up with arXiv anymore, why introduce any more sources of long reads? Sure,...","categories": ["squib"],
+        "tags": ["academia"],
+        "url": "/2019/why-blog/",
+        "teaser": null
+      },{
+        "title": "How the Transformers broke NLP leaderboards",
+        "excerpt":"This post summarizes some of the recent XLNet-prompted discussions on Twitter and offline. Idea credits go to Yoav Goldberg, Sam Bowman, Jason Weston, Alexis Conneau, Ted Pedersen, fellow members of Text Machine Lab, and many others. Any misconfiguration of those ideas is my own. A big reason why NLP is...","categories": ["squib"],
+        "tags": ["academia","methodology"],
+        "url": "/2019/leaderboards/",
+        "teaser": null
+      },{
+        "title": "On word analogies and negative results in NLP",
+        "excerpt":"In real world, fake news spread faster than facts. People’s attention is caught by sensational, exaggerated, clickbait-y messages like “5.23 million more immigrants are moving to the UK”. Any subsequent fact-checking messages look less sensational and they will not reach as many people. Once the damage is done, it’s done....","categories": ["squib"],
+        "tags": ["academia","methodology","negresults","review"],
+        "url": "/2019/analogies/",
+        "teaser": null
+      },{
+        "title": "Talking to people outside your echo chamber: SocNLP challenges",
+        "excerpt":"This blog is NOT political, even though the story below will show some of my political views. I’m sharing it because it highlights a burning issue in computational social science that is getting nowhere near enough attention. Reader beware. Academics and Populism Like most researchers I know, I’ve been politically...","categories": ["squib"],
+        "tags": ["academia","socialNLP"],
+        "url": "/2019/conversation/",
+        "teaser": null
+      },{
+        "title": "How to teach NLP to non-CS-majors in 2 weeks?",
+        "excerpt":"I strongly believe that getting machines to understand natural language, if at all possible, will require much interdisciplinary collaboration. It’s not clear whether NLP models should be biologically plausible or explicitly encode any linguistic structures – but we do know that language is a very complex thing, and no discipline...","categories": ["squib"],
+        "tags": ["academia","teaching"],
+        "url": "/2019/nlp4linguists/",
+        "teaser": null
+      },{
+        "title": "Peer review in NLP: reject-if-not-SOTA",
+        "excerpt":"Everything wrong with reject-if-not-SOTA After each reviewing round for a major conference, #NLProc Twitter erupts with bitter reports of methods rejected for failing to achieve the state-of-the-art status (SOTA). Another @emnlp2019 reviewer&#39;s 3-line review concludes: &quot;The main weakness of the paper is the results do not beat the state of...","categories": ["squib"],
+        "tags": ["academia","methodology","peer-review"],
+        "url": "/2020/reviewing-models/",
+        "teaser": null
+      },{
+        "title": "Peer review in NLP: resource papers",
+        "excerpt":"Dangerous preconceptions about resource papers Most success stories in NLP are about supervised or semi-supervised learning. Fundamentally, that means that our parsers, sentiment classifiers, QA systems and everything else are only as good as the training data, and that fact makes data and model engineering equally important for further progress....","categories": ["squib"],
+        "tags": ["academia","methodology","peer-review"],
+        "url": "/2020/reviewing-data/",
+        "teaser": null
+      },{
+        "title": "Should the reviewers know who the authors are?",
+        "excerpt":"This post is my $0.02 in the ongoing debate about ACL peer review reform. TLDR if you’ve missed that: ACL is discussing the ways to improve peer review, and released short-term and long-term review proposals that generated many lively discussions on Twitter and in ACL 2020. The debate is still...","categories": ["squib"],
+        "tags": ["academia","peer-review"],
+        "url": "/2020/anonymity/",
+        "teaser": null
+      },{
+        "title": "How to Record a Virtual Conference Talk",
+        "excerpt":"Virtual talks have been the bliss and the curse of NLP virtual conferences. Because of the efforts to preserve them and link them up to the paper pages in ACL anthology, they are the bliss for the audience, and greatly improve conference accessibility worldwide. But for the authors they are...","categories": ["howto"],
+        "tags": ["academia,","productivity"],
+        "url": "/2021/recording/",
+        "teaser": null
+      }]
